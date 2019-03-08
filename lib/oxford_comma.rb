13f1ad424cb806b944f7a90dc5f4e42ad28fee5e
Gregory Losco <gtlosco@gmail.com>
2019-03-08 04:34:02 +0000

    Automatically backed up by Learn

--- lib/oxford_comma.rb
+++ lib/oxford_comma.rb
@@ -1,10 +1,4 @@
 def oxford_comma(array)
-  return "kiwi"
-  array.each do { |fruit| ","}
-end
-end
-
-def oxford_comma(array)
   if array.length == 2
     return "#{array[0]} and #{array[1]}"
   elsif 2 < array.length
